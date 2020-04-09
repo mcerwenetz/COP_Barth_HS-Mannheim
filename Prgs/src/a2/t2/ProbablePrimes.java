fcfcc4842afdb38efa60995f2163982df958b6f9
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-04-10 00:18:37 +0200

    Charge implementiert
    Das Messen passiert nun in Chargen an Threads. Auf die weise kann man
    auch große Strecken durchlaufen ohne dass gleich der Heap volläuft weil
    die Strecke jetzt eingeteilt ist.
    
    Comments added.
    
    Nächste Stufe wäre ein richtiger pool an threads der automatisch threads
    hinzufügt wenn ein alter fertig ist. Dann muss nicht immer alle 8
    Threads alles warten bis alle fertig sind.
    
    Noch eine Idee wäre eine Art Thread producer und eine Art Thread
    Consumer mit einer Bounded Queue in der Mitte damit die Last im Zaum
    gehalten wird.

--- Prgs/src/a2/t2/ProbablePrimes.java
+++ Prgs/src/a2/t2/ProbablePrimes.java
@@ -9,28 +9,51 @@ import util.Util;
 class ProbablePrime {
 
 	public static void main(String[] args) {
-		BigIntHolder zaehler = new BigIntHolder(new BigInteger("1"));
-		BigIntHolder upperlimit = new BigIntHolder(BigInteger.TEN.pow(8));
+		// zaehler für die einzelnen chargen an Threads
+		BigInteger chargenzaehler = new BigInteger("0");
+		// Upperlimit der primetests
+		BigIntHolder upperlimit = new BigIntHolder(BigInteger.TEN.pow(5));
+		// Anzahl an Primzahlen in eigenem Datenformat damit es geteilt werden kann
 		IntHolder primes = new IntHolder(0);
-		int threadcount = upperlimit.getB().intValue() - zaehler.getB().intValue();
+		// lock damit nicht gleichzeitig auf primes zugegriffen werden kann
 		Lock lock = new ReentrantLock();
-		MySemaphore sem = new MySemaphore(16);
-		
-		Thread[] primeThreads = new Thread[threadcount];
-		
-		
-		for (BigInteger i = BigInteger.ZERO; i.doubleValue() < primeThreads.length; i=i.add(BigInteger.ONE)) {
-			primeThreads[i.intValue()] = new PrimeCounterThreadSingle(i, primes, lock, sem);
-		}
+		// cores sind die Anzahl der Prozessorkerne. Auf dem surface sind das 8. So groß
+		// werden die einzelnen Thread-Chargen
+		Integer cores = Runtime.getRuntime().availableProcessors();
+		/*
+		 * Semaphore. Es können maximal cores Threads erstellt werden. Unnötig wenn man
+		 * in chargen arbeitet die so groß sind wie das System cores hat. Wenn cores
+		 * heißt wie viele Threads gleichzeitig starten können. Die Semaphore ist der
+		 * Flaschenhals
+		 */
+		MySemaphore sem = new MySemaphore(cores);
+		Integer chargengroesse = 100;
 
-		for (var i : primeThreads) {
-			i.start();
-		}
+		// Threadarray ist cores groß. Also eine Charge.
+		Thread[] primeThreads = new Thread[chargengroesse];
 
-		for (var i : primeThreads) {
-			Util.join(i);
+		// solange der Zaehler das Limit nicht erreicht hat wird er inkrementiert um
+		// cores (8) pro Charge
+		while (chargenzaehler.intValue() < upperlimit.getB().intValue()) {
+			// lokale Zählvariable = chargenzaehler. Für alle Slots im Array wird ein Thread
+			// erstellt.
+			for (BigInteger i = chargenzaehler; i.doubleValue() < chargenzaehler.intValue() + chargengroesse; i = i
+					.add(BigInteger.ONE)) {
+				// pos ist modulo damit es keinen overflow gibt. Sonst i ziemlich groß und
+				// findet die pos im Array nicht mehr.
+				primeThreads[i.intValue() % chargengroesse] = new PrimeCounterThreadSingle(i, primes, lock, sem);
+			}
+			// Alle Threads in einer charge starten
+			for (var i : primeThreads) {
+				i.start();
+			}
+			// Alle Threads in einer charge joinen
+			for (var i : primeThreads) {
+				Util.join(i);
+			}
+			// zaehler um charge erhöhen
+			chargenzaehler = chargenzaehler.add(new BigInteger(chargengroesse.toString()));
 		}
-		
 		System.out.println(primes);
 	}
 
