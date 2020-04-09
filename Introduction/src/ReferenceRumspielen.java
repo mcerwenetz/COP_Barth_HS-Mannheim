858c786eca383bcc6f79892fe9b3e13c4e251ec2
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-04-09 21:56:20 +0200

    Beispiel für Pass-By-Referenz hinzugefügt

--- Introduction/src/ReferenceRumspielen.java
+++ Introduction/src/ReferenceRumspielen.java
@@ -1,5 +1,7 @@
 import java.math.BigInteger;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class ReferenceRumspielen {
@@ -8,16 +10,31 @@ public class ReferenceRumspielen {
 	
 	public static void main(String[] args) {
 		BigInteger aBigInteger = new BigInteger("0");
-		Integer aInteger = new Integer(0);
+		Integer aInteger = 0;
 		LinkedList<Integer> a = new LinkedList<Integer>();
 		a.add(0);
+		IntegerHolder inthold = new IntegerHolder(0);
+		ReentrantLock l = new ReentrantLock();
 
 		foo(aBigInteger);
 		foo2(aInteger);
 		foo3(a);
+		foo4(inthold);
+		foo5(l);
+		
 		System.out.println(aBigInteger.toString());
 		System.out.println(aBigInteger.toString());
 		System.out.println(a.toString());
+		System.out.println(inthold.getInteger().toString());
+	}
+
+	private static void foo5(ReentrantLock l) {
+		l.lock();
+		l.unlock();
+	}
+
+	private static void foo4(IntegerHolder inthold) {
+		inthold.add(1);
 	}
 
 	private static void foo3(LinkedList<Integer> a) {
@@ -26,6 +43,8 @@ public class ReferenceRumspielen {
 	}
 
 	private static void foo2(Integer aInteger) {
+//		Integer bInteger = new Integer(aInteger.intValue())
+//		aInteger = bInteger;
 		aInteger++;
 		
 	}
