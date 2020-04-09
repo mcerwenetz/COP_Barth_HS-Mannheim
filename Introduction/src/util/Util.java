fbc60d890b4d09e2a16147af1c2eac72f9019d41
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-04-09 21:55:08 +0200

    Added Helper
    Utils zum Rumspielen hinzugefügt.

---
+++ Introduction/src/util/Util.java
@@ -0,0 +1,34 @@
+package util;
+
+public class Util {
+	private static long nanoSecs;
+	public final static void resetTime() {
+		nanoSecs = System.nanoTime();
+	}
+	
+	public final static long getTimeMicros() {
+		long current = System.nanoTime();
+		return (current-nanoSecs)/1000;
+	}
+	
+	public final static long getTimeMilis() {
+		long current = System.nanoTime();
+		return (current-nanoSecs)/1000000;
+	}
+	
+	public final static void sleep(long millisecs) {
+		try{
+			Thread.sleep(millisecs);
+		}catch(InterruptedException e) {
+			System.out.println("Util.sleep: Interrupted");
+		}
+	}
+	
+	public final static void join(Thread d) {
+		try {
+			d.join();
+		} catch (InterruptedException e) {
+			System.out.println("Util.join: Interrupt joining");
+		}
+	}
+}
