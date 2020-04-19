6e9da224ffec80047ae34b4292b3b00452f88f25
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-04-19 19:57:00 +0200

    locks beim producer entfernt. BlockingQueue macht das schon.

--- Prgs/src/a2/t4/ProducerThread.java
+++ Prgs/src/a2/t4/ProducerThread.java
@@ -2,32 +2,27 @@ package a2.t4;
 
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import util.Util;
 
 public class ProducerThread extends Thread {
-	
-	BlockingQueue<Integer> queue;
-	Lock prodLock;
 
-	public ProducerThread(BlockingQueue<Integer> list, Lock lock) {
-		this.queue=list;
-		this.prodLock=lock;
+	BlockingQueue<Integer> queue;
+	AtomicBoolean prodDone;
+
+	public ProducerThread(BlockingQueue<Integer> list, AtomicBoolean prodDone) {
+		this.queue = list;
+		this.prodDone = prodDone;
 	}
 
-		@Override
-		public void run() {
-			Random rand = new Random();
-			for(int i = 0; i < 200; i++) {
-				Integer a = Math.abs(rand.nextInt()%400);
-				prodLock.lock();
-				try {
-					queue.add(a);
-				} finally {
-					prodLock.unlock();
-				}
-				Util.sleep(4);
-			}
+	@Override
+	public void run() {
+		Random rand = new Random();
+		for (int i = 0; i < 100; i++) {
+			Integer a = Math.abs(rand.nextInt() % 400);
+			queue.add(a);
+			Util.sleep(4);
 		}
+	}
 }
