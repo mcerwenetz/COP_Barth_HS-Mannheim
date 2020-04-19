ec3e49f45660bda9f0783f8b9c25d0b33bbe1df5
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-04-19 19:58:36 +0200

    ConsumerThread ist eine neue Klasse und ersetzt Odd und EvenConsumer
    Früher brauchte man beide um die Summen korrekt zuzuordnen. So spart man
    sich eine Klasse.

---
+++ Prgs/src/a2/t4/ConsumerThread.java
@@ -0,0 +1,37 @@
+package a2.t4;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import util.Sum;
+
+public class ConsumerThread extends Thread {
+
+	AtomicBoolean conDone;
+	BlockingQueue<Integer> consumerQueue;
+	Sum sum;
+
+	public ConsumerThread(AtomicBoolean conDone, BlockingQueue<Integer> consumerQueue, Sum sum) {
+		super();
+		this.conDone = conDone;
+		this.consumerQueue = consumerQueue;
+		this.sum = sum;
+	}
+
+	@Override
+	public void run() {
+		Integer integer = 0;
+		while (!conDone.get() || !consumerQueue.isEmpty()) {
+			try {
+				integer = consumerQueue.poll(200, TimeUnit.MILLISECONDS);
+				if (integer == null)
+					continue;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			sum.increaseBy(integer);
+		}
+	}
+
+}
