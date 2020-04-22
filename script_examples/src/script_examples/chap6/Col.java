7e25793998fcf4eb7f18725fd038105868a8b11b
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-04-22 19:47:07 +0200

    Interleaving-Problem beseitigt.
    Abschnitt ist jetzt synchronized im runnable.

--- script_examples/src/script_examples/chap6/Col.java
+++ script_examples/src/script_examples/chap6/Col.java
@@ -11,8 +11,10 @@ public class Col {
 		List<Integer> list = Collections.synchronizedList(new LinkedList<Integer>());
 		Runnable drain = () -> {
 			try {
-				while(!list.isEmpty()) {
-					list.remove(0);
+				synchronized (list) {
+					while (!list.isEmpty()) {
+						list.remove(0);
+					}
 				}
 			} catch (IndexOutOfBoundsException e) {
 				System.out.println("Ouch\n");
@@ -23,7 +25,7 @@ public class Col {
 		}
 		Thread[] ts = new Thread[10];
 		for (int i = 0; i < ts.length; i++) {
-			ts[i]= new Thread(drain);
+			ts[i] = new Thread(drain);
 			ts[i].start();
 		}
 		Util.joinall(ts);
