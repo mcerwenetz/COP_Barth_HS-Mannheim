35b83d03f08344c82eb1f68b9659e94b7c750610
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-04-23 19:17:01 +0200

    Beispiel WithinThreadExecutor hinzugefügt.
    Ein Executor führt 4 mal nacheinander einen Task aus in dem er immer
    kürzer schläft.

---
+++ script_examples/src/script_examples/chap7/WithinThreadExecutor.java
@@ -0,0 +1,36 @@
+package script_examples.chap7;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+import script_examples.Util;
+
+public class WithinThreadExecutor implements Executor {
+
+	@Override
+	public void execute(Runnable command) {
+		command.run();
+	}
+
+	public static void main(String[] args) {
+		Executor executor = new WithinThreadExecutor();
+		CountDownLatch latch = new CountDownLatch(4);
+		Util.resetTime();
+
+		for (int i = 4; i >= 1; i--) {
+			final int j = i;
+			Runnable cmd = () -> {
+				System.out.println(j * 200 + " msecs");
+				Util.sleep(j * 200);
+				latch.countDown();
+			};
+			executor.execute(cmd);
+		}
+		try {
+			latch.await();
+		} catch (InterruptedException ignored) {
+		}
+		System.out.println(Util.getTimeMilis() + " msecs total");
+	}
+
+}
