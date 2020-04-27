33f63a9afcc42126daf3c7d5cd3211d59ac9e592
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-04-27 19:25:01 +0200

    Zweiter Test implementiert
    Ein Threadpool aus 10 Threads kümmert sich um die Execution von
    Callables die einem ExecutionerService gegeben werden.
    In den Callables wird ein Integer eingefügt und ein latch runtergezählt
    auf das im Main-Thread gewartet wird. Sonst macht der Main-Thread
    einfach weiter obwohl die Threads noch gar nicht fertig sind.

--- Prgs/src/a3/t2/BenchMap.java
+++ Prgs/src/a3/t2/BenchMap.java
@@ -6,12 +6,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BenchMap {
 	
 	public static void main(String[] args) {
-		HashMap<Integer, Integer> hashMap = new HashMap<>();
 		Map<Integer, Integer> synchronizedHashMap = Collections.synchronizedMap(new HashMap<Integer, Integer>());
 		ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap<>();
 		
@@ -19,31 +24,39 @@ public class BenchMap {
 		List<Integer> intList = new ArrayList<Integer>();
 		int size=1_000_000;
 		
+		CountDownLatch latch = new CountDownLatch(size);
+		ExecutorService threadpool = Executors.newFixedThreadPool(10);
+		ExecutorCompletionService<Void> service = new ExecutorCompletionService<>(threadpool);
+		
 		for (int i = 0; i < size; i++) {
 			intList.add(random.nextInt());
 		}
 		
-		util.Util.resetTime();
-		fillMap(intList, hashMap);
-		emptyMap(intList, hashMap);
-		Long timeHashmap = util.Util.getTimeMilis();
-		
 		
 		util.Util.resetTime();
-		fillMap(intList, synchronizedHashMap);
+		fillMap(intList, synchronizedHashMap, service, latch);
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		emptyMap(intList, synchronizedHashMap);
 		Long timeSynchronizedMap = util.Util.getTimeMilis();
 		
 		util.Util.resetTime();
-		fillMap(intList, concurrentHashMap);
+		fillMap(intList, concurrentHashMap, service, latch);
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		emptyMap(intList, concurrentHashMap);
 		Long timeConcurrentHashMap = util.Util.getTimeMilis();
 		
 		
-		System.out.println("This took " + timeHashmap + " ms on the HashMap");
 		System.out.println("This took " + timeSynchronizedMap + "ms on the synchronized HashMap");
 		System.out.println("This took " + timeConcurrentHashMap + "ms on the concurrent HashMap");
-		
 		
 	}
 
@@ -53,10 +66,15 @@ public class BenchMap {
 		}
 	}
 
-	private static void fillMap(List<Integer> intList, Map<Integer, Integer> map) {
+	private static void fillMap(List<Integer> intList, Map<Integer, Integer> map, ExecutorCompletionService<Void> service, CountDownLatch latch) {
 		Random random = new Random();
 		for (Integer integer : intList) {
-			map.put(integer, intList.get(random.nextInt(intList.size()-1)));
+			Callable<Void> callable = () -> {
+				map.put(integer, intList.get(random.nextInt(intList.size()-1)));
+				latch.countDown();
+				return null;
+			};
+			service.submit(callable);
 		}
 	}
 
