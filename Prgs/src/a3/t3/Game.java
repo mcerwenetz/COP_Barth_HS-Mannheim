e9205aef8661eb26209071d9845d6ba3e848dddc
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-04-30 23:09:34 +0200

    Game hinzugefügt.
    Ein Game umfasst zwei Player. Nach dem ein Game gelaufen ist wird der
    Player mit dem größten Score als Sieger des Games gekürt.
    Ein Boolean gameFinished wird bei einem Remis als Ergebnis zweier Rolls
    der Player gebraucht. Ein Spiel gilt erst als Beendet sobald ein Spieler
    eine höhere Zahl würfelt.
    
    Nachdem der Sieger feststeht werden seine gewonnenen Runden um 1 erhöht.

---
+++ Prgs/src/a3/t3/Game.java
@@ -0,0 +1,45 @@
+package a3.t3;
+
+public class Game {
+	Player player1;
+	Player player2;
+	Player winner;
+	Boolean gameFinished=Boolean.FALSE;
+	
+	public Game(Player player1, Player player2) {
+		super();
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+
+
+	public Player getWinner() {
+		return winner;
+	}
+
+
+
+	public void setWinner(Player winner) {
+		this.winner = winner;
+	}
+
+
+	public void startGame() {
+		gameFinished=false;
+		
+		while (gameFinished==false) {
+			player1.roll();
+			player2.roll();
+			if (player1.getScore()>player2.getScore()){
+				winner=player1;
+				gameFinished=true;
+			}else if (player2.getScore() > player1.getScore()) {
+				winner=player2;
+				gameFinished=true;
+			}
+		}
+		winner.incRoundsWon();
+	}
+
+}
