54df98e59d91c0818c74914995177d3531871b69
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-05-06 11:42:06 +0200

    Exceptionhandling zugefügt.

--- Prgs/src/a3/t4/Main.java
+++ Prgs/src/a3/t4/Main.java
@@ -1,19 +1,25 @@
 package a3.t4;
 
+import java.util.concurrent.RejectedExecutionException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		MyExecutor myExecutor = new MyExecutor(4);
+		MyExecutor myExecutor = new MyExecutor(1);
 		
 		Runnable r = new Runnable() {
 			public void run() {
 				System.out.println("running");
 			}
 		};
-		
+		myExecutor.doBlock(false);
 		for (int i = 0; i < 1000; i++) {
-			myExecutor.execute(r);
+			try {
+				myExecutor.execute(r);
+			} catch (RejectedExecutionException e) {
+				System.out.println("Queue full");
+			}
 		}
 		System.out.println("Limit reached " + myExecutor.limitReached + " times");
 	}
--- Prgs/src/a3/t4/MyExecutor.java
+++ Prgs/src/a3/t4/MyExecutor.java
@@ -1,6 +1,7 @@
 package a3.t4;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyExecutor implements Executor {
@@ -8,18 +9,29 @@ public class MyExecutor implements Executor {
 	Integer maxThreads;
 	AtomicInteger runningThreads;
 	Integer limitReached=0;
+	private Boolean doBlock;
 
 	public MyExecutor(Integer maxThreads) {
 		this.maxThreads = maxThreads;
 		this.runningThreads = new AtomicInteger(0);
+		doBlock=true;
+	}
+	
+	public void doBlock(Boolean block) {
+		this.doBlock = block;
 	}
 
 	@Override
 	public void execute(Runnable command) {
 		if (runningThreads.get() >= maxThreads) {
+			if(doBlock) {
 //			System.out.println("Queue full");
 			limitReached++;
 			return;
+			}
+			else {
+				throw new RejectedExecutionException();
+			}
 		} else {
 			Thread t = new Thread(command);
 			t.start();
