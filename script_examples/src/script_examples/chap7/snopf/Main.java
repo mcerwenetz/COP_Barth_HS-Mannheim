b7ddf5e793b94be64af9cc7194582fa8fb73e7d7
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-05-19 18:56:29 +0200

    Main für SNOPF

---
+++ script_examples/src/script_examples/chap7/snopf/Main.java
@@ -0,0 +1,25 @@
+package script_examples.chap7.snopf;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import script_examples.Util;
+import script_examples.chap7.snopf.Primes;
+
+public class Main {
+	public static void main(String[] args) {
+		List<BigInteger> ns = new ArrayList<BigInteger>();
+		int start=1_000_000;
+		int no = 256;
+		for (int i = start; i < start+no; i++) {
+			String sval= Integer.valueOf(i).toString();
+			ns.add(new BigInteger(sval));
+		}
+		
+		Util.resetTime();
+		BigInteger snopf = Primes.seqSnopf(ns);
+		long used = Util.getTimeMilis();
+		System.out.println("Sequenzielles Snopf braucht " + used + "ms");
+	}
+}
