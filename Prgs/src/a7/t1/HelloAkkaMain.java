28cf453d2bea60960eaed0c82d20ab39024e0392
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-06-27 19:01:41 +0200

    Kommentare hinzugefügt

--- Prgs/src/a7/t1/HelloAkkaMain.java
+++ Prgs/src/a7/t1/HelloAkkaMain.java
@@ -12,10 +12,14 @@ import akka.actor.Props;
 public class HelloAkkaMain {
 	
 	public static void main(String[] args) {
+		//Standard Config erstellen
 		Config akkaConf = ConfigFactory.load();
+		//Erstelle ein ActorSystem
 		ActorSystem system = ActorSystem.create("System", akkaConf);
+		//Erstelle einen Actor helloAkka der dann später den GreeterAkka erstellt
 		ActorRef helloAkka = system.actorOf(Props.create(HelloAkka.class), "HelloAkka");
 		Util.sleep(1000);
+		//Fahre das System herunter
 		system.terminate();
 	}
 
