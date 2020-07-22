81869b69644553bf7974fa7cc8e8efe8e8627ae5
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-07-22 21:52:52 +0200

    Jetzt wird eine Liste an Callables übergeben und mit invokeAll alle ausgeführt.

--- script_examples/src/script_examples/chap7/threadPool/FixedTP.java
+++ script_examples/src/script_examples/chap7/threadPool/FixedTP.java
@@ -2,10 +2,9 @@ package script_examples.chap7.threadPool;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 public class FixedTP {
@@ -13,19 +12,31 @@ public class FixedTP {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		ExecutorService threadpool = Executors.newCachedThreadPool();
-		List<Future<Void>> futureList  = new LinkedList<Future<Void>>();
 
+		List<Callable<Void>> callables = new LinkedList<Callable<Void>>();
+		
 		for (int i = 0; i < 3; i++) {
-			futureList.add((Future<Void>) threadpool.submit(() -> {
-				System.out.println("Hello from Runnable");
-				try {
+		
+			Callable<Void> callable = new Callable<Void>() {
+
+				@Override
+				public Void call() throws Exception {
+					System.out.println("Hello from Callable");
 					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					return null;
 				}
-			}));
+			};
+
+			callables.add(callable);
 		}
+		
+		try {
+			threadpool.invokeAll(callables);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+			 
 		try {
 			threadpool.awaitTermination(2, TimeUnit.SECONDS);
 		} catch (InterruptedException e) {
@@ -33,8 +44,5 @@ public class FixedTP {
 			e.printStackTrace();
 		}
 		threadpool.shutdown();
-		for (Future<Void> future : futureList) {
-			System.out.println(future);
-		}
 	}
 }
