ccb09578f9d2b6c9afce328674e6af1b65c998fe
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-07-22 21:44:04 +0200

    Futures werden jetzt abgefangen und ausgegeben

--- script_examples/src/script_examples/chap7/threadPool/FixedTP.java
+++ script_examples/src/script_examples/chap7/threadPool/FixedTP.java
@@ -1,24 +1,40 @@
 package script_examples.chap7.threadPool;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 public class FixedTP {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		ExecutorService threadpool = Executors.newCachedThreadPool();
+		List<Future<Void>> futureList  = new LinkedList<Future<Void>>();
 
-		for (int i = 0; i < 100; i++) {
-			threadpool.submit(() -> {
+		for (int i = 0; i < 3; i++) {
+			futureList.add((Future<Void>) threadpool.submit(() -> {
 				System.out.println("Hello from Runnable");
 				try {
-					Thread.sleep(100);
+					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			});
+			}));
+		}
+		try {
+			threadpool.awaitTermination(2, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		threadpool.shutdown();
+		for (Future<Void> future : futureList) {
+			System.out.println(future);
+		}
 	}
 }
